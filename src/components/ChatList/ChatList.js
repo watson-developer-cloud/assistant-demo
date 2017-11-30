728a0f5aee098e66d44a659196b145560f609a02
repjarms <jtarn@us.ibm.com>
2017-11-30 15:49:29 -0500

    raise state up to <App />

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -1,88 +1,19 @@
 import React from 'react';
-import { InputWithButton } from 'watson-react-components';
+import PropTypes from 'prop-types';
 import GenericMessage from '../GenericMessage/GenericMessage';
 
-class ChatList extends React.Component {
-  constructor(props) {
-    super(props);
+const ChatList = ({ messages }) => (
+  <div className="ibm-lg-col-4 ibm-padding chat-list">
+    {messages.map(message => (
+      <GenericMessage
+        sender={message.sender}
+        text={message.text || ''}
+      />))}
+  </div>
+);
 
-    this.state = {
-      messages: [],
-      lastMessageJson: '',
-      lastMessageContext: {},
-    };
-  }
-
-  componentWillMount() {
-    this.fetchMessage();
-  }
-
-  updateChatList(sender, text) {
-    this.setState({ messages: [...this.state.messages, { sender, text }] });
-  }
-
-  updateJsonSidebar(json) {
-    this.setState({ lastMessageJson: json });
-  }
-
-  updateConversationContext(contextObj) {
-    this.setState({ lastMessageContext: contextObj });
-  }
-
-  fetchMessage(text) {
-    // construct conversation payload
-    const payload = {
-      input: { text },
-      context: this.state.lastMessageContext,
-    };
-
-    // add user message to state
-    this.updateChatList('user', text);
-
-    // send payload to conversation
-    fetch('/api/message', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    }).then((data) => {
-      data.json()
-        .then((json) => {
-          // update chat list with conversation response
-          this.updateChatList('bot', json.output.text[0]);
-
-          // send stringified JSON to sidebar
-          this.updateJsonSidebar(JSON.stringify(json));
-
-          // update context
-          this.updateConversationContext(json.context);
-        });
-    });
-  }
-
-  render() {
-    return (
-      <div className="ibm-lg-col-4 ibm-padding chat-list">
-        {this.state.messages.map(message => (
-          <GenericMessage
-            sender={message.sender}
-            text={message.text || ''}
-          />))}
-
-        <InputWithButton
-          className="chat-input"
-          onSubmit={(e) => {
-            this.fetchMessage(e.target.value);
-            e.target.value = '';
-          }}
-          placeholder="Type here..."
-        />
-        {this.state.lastMessageJson}
-      </div>
-    );
-  }
-}
+ChatList.propTypes = {
+  messages: PropTypes.array.isRequired,
+};
 
 export default ChatList;
