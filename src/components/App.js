6933eb169eb30db75c61983d783695fe4065f13f
repjarms <jtarn@us.ibm.com>
2017-11-30 11:21:56 -0500

    import sidebar components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ChatContainer from './ChatContainer/ChatContainer';
+import SelectionSidebar from './SelectionSidebar/SelectionSidebar';
+import JsonSidebar from './JsonSidebar/JsonSidebar';
 
 const App = () => (
   <div className="ibm">
+    <SelectionSidebar />
     <ChatContainer />
+    <JsonSidebar />
   </div>
 );
 
