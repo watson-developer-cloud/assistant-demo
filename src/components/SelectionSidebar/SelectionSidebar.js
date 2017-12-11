d5a71d1c5dd2633d35c395de1c4a4526ab70f48d
repjarms <jtarn@us.ibm.com>
2017-12-11 14:03:37 -0500

    add modal overlay of selection sidebar

--- src/components/SelectionSidebar/SelectionSidebar.js
+++ src/components/SelectionSidebar/SelectionSidebar.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Modal } from 'watson-react-components';
+import GenericButton from '../GenericButton/GenericButton';
 
-const SelectionSidebar = () => (
-  <div className="ibm-col-lg-4">
-    SelectionSidebar
-  </div>
-);
+class SelectionSidebar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalVisible: false,
+    };
+  }
+
+  onExit() {
+    this.setState({ modalVisible: false });
+  }
+
+  onEnter() {
+    this.setState({ modalVisible: true });
+  }
+
+  render() {
+    return (
+      <div className="ibm-col-lg-4 ibm-col-sm-0">
+        <GenericButton
+          onClick={() => { this.onEnter(); }}
+        />
+        <Modal
+          isOpen={this.state.modalVisible}
+          onExit={() => { this.onExit(); }}
+          onEnter={() => { this.onEnter(); }}
+        >
+          <div className="selection-sidebar__modal-content ibm-col-lg-8">
+            <p>In this demo, Watson Conversation has been trained on specific banking capabilities. Choose one of the 4 scenarios to explore.</p>
+          </div>
+          </Modal>
+      </div>
+    );
+  }
+}
 
 export default SelectionSidebar;
