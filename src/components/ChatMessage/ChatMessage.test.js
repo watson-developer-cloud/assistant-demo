45f4a17f0ad51df8aff0ab77292d9e8e5d2ddf9c
repjarms <jtarn@us.ibm.com>
2018-01-10 17:17:42 -0500

    add batch of component tests

--- src/components/ChatMessage/ChatMessage.test.js
+++ src/components/ChatMessage/ChatMessage.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import ChatMessage from './ChatMessage';
 
-describe('GenericMessage', () => {
+const sandbox = sinon.createSandbox();
+
+describe('ChatMessage', () => {
+  // run each test in isolation
+  afterEach(() => sandbox.restore());
+
   it('renders bot message', () => {
     const component = shallow(<ChatMessage type="bot" text="hello" />);
     expect(component.find('p').text()).toEqual('hello');
@@ -10,5 +16,9 @@ describe('GenericMessage', () => {
   it('renders user message', () => {
     const component = shallow(<ChatMessage type="user" text="hello" />);
     expect(component.find('p').text()).toEqual('hello');
+  });
+  it('renders chat option', () => {
+    const component = shallow(<ChatMessage type="option" text="Texas" />);
+    expect(component.find('button').text()).toEqual('Texas');
   });
 });
