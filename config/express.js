cca9e470cd7e198179cd11ead9e76f8decb2a33a
repjarms <jtarn@us.ibm.com>
2018-01-12 09:31:47 -0500

    serve gzipped css bundle in production

--- config/express.js
+++ config/express.js
@@ -38,9 +38,18 @@ module.exports = (app) => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
+    // in production, serve gzipped js bundle
     app.get('*.js', (req, res, next) => {
       req.url = `${req.url}.gz`;
       res.set('Content-Encoding', 'gzip');
+      next();
+    });
+
+    // in production, serve gzipped css bundle
+    app.get('*.css', (req, res, next) => {
+      req.url = `${req.url}.gz`;
+      res.set('Content-Encoding', 'gzip');
+      res.set('Content-Type', 'text/css');
       next();
     });
   }
