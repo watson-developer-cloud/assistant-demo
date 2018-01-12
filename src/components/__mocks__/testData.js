5dc5ee65574b901113ded1fdd7ca1219873c26e9
repjarms <jtarn@us.ibm.com>
2018-01-12 11:23:15 -0500

    mock test data in central location

---
+++ src/components/__mocks__/testData.js
@@ -0,0 +1,4 @@
+export const mockSidebarJson = JSON.stringify({ test: 'hi' });
+export const mockBotChatMessage = { type: 'bot', content: 'hello' };
+export const mockUserChatMessage = { type: 'user', content: 'hello' };
+export const mockOptionsChatMessage = { type: 'options', content: [{ label: 'Texas', value: 'texas' }] };
