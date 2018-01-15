576199e4decf27eebd4bd4500b0ab167a9fefb49
repjarms <jtarn@us.ibm.com>
2018-01-15 12:11:34 -0500

    add ChatOptionList tests

---
+++ src/components/ChatOptionList/ChatOptionList.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import ChatOptionList from './ChatOptionList';
+
+const sandbox = sinon.createSandbox();
+const onUserInputSpy = sinon.spy();
+const chatOptionStubData = [
+  { label: 'Texas', value: 'texas' },
+  { label: 'New York', value: 'newyork' },
+  { label: 'California', value: 'california' },
+];
+
+describe('ChatOptionList', () => {
+  // run each test in isolation
+  afterEach(() => sandbox.restore());
+
+  it('renders the correct number of ChatOptions', () => {
+    const component = shallow(<ChatOptionList
+      options={chatOptionStubData}
+      onUserInput={onUserInputSpy}
+    />);
+    expect(component.find('div').children()).toHaveLength(3);
+  });
+});
