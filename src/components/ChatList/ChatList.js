64225cb381622592ab3f0df8631ae487aa9b24ba
repjarms <jtarn@us.ibm.com>
2018-01-31 17:15:38 -0500

    expand Chat types

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -2,24 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ChatMessage from '../ChatMessage/ChatMessage';
 import ChatOptionList from '../ChatOptionList/ChatOptionList';
+import BalanceCard from '../BalanceCard/BalanceCard';
+import ApptCard from '../ApptCard/ApptCard';
+
 
 const ChatList = ({ messages, onUserInput }) => (
   <div className="ibm-lg-col-4 ibm-padding chat-list">
     {messages.map((message) => {
-      if (message.type === 'option') {
-        return (
-          <ChatOptionList
-            options={message.content}
-            onUserInput={onUserInput}
-          />
-        );
+      switch (message.type) {
+        case 'option':
+          return (
+            <ChatOptionList
+              options={message.content}
+              onUserInput={onUserInput}
+            />
+          );
+        case 'balance':
+          return (
+            <BalanceCard />
+          );
+        case 'appointment':
+          return (
+            <ApptCard />
+          );
+          /*
+        case 'agent':
+          return (
+
+          );
+          */
+        default:
+          return (
+            <ChatMessage
+              type={message.type}
+              text={message.content || ''}
+            />
+          );
       }
-      return (
-        <ChatMessage
-          type={message.type}
-          text={message.content || ''}
-        />
-      );
     })}
   </div>
 );
