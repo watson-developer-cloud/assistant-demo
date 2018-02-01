a7ad2fbb6fbbcbb4bdc517e013451d758cc6847a
Kenny Lam <lkenny@us.ibm.com>
2018-02-01 17:01:07 -0500

    updated PathSelection card css to latest creative

--- src/components/PathSelectionCard/PathSelectionCard.js
+++ src/components/PathSelectionCard/PathSelectionCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Icon from '../Icon/Icon';
 
 const PathSelectionCard = ({ label, path, onClick }) => (
   <button
     className="path-selection-card ibm-type-c"
     onClick={() => { onClick(path); }}
   >
-    {label}
+    <p className="ibm-type-c">{label}</p>
+    <Icon type="circle-arrow-right" size={32} />
   </button>
 );
 
