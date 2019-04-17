0d192678fad1003d257c45c84975caa3ffa3e691
German Attanasio <germanattanasio@gmail.com>
2019-04-17 09:31:46 -0400

    chore: ignore tests if not credentials

--- test/unit/express.test.js
+++ test/unit/express.test.js
@@ -1,22 +1,20 @@
 process.env.NODE_ENV = 'production';
 const request = require('supertest');
 require('dotenv').config({ silent: true });
-const app = require('../../index');
+
 
 const describeOrSkip = process.env.ASSISTANT_ID ? describe : describe.skip;
 describeOrSkip('express', () => {
+  const app = require('../../index');
   it('should load home page when GET /', (done) => {
     request(app).get('/').expect(200, done);
   });
 
   it('should respond with JSON when GET /api/session', (done) => {
-    if (process.env.ASSISTANT_ID) {
-      return request(app)
-        .get('/api/session')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
-    }
-    done();
+    request(app)
+      .get('/api/session')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
   });
 });
