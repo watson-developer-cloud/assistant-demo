bb6ee622d1d6fb4f1242c9cc8e28cc0b44dadd7e
Steve Green <stevenpkg@au1.ibm.com>
2019-05-08 12:24:26 +1000

    chore(sdk): Updated watson sdk
    * Update watson SDK to ibm-watson@4.1.0
    * modificed unit test

--- test/unit/express.test.js
+++ test/unit/express.test.js
@@ -1,20 +1,35 @@
 process.env.NODE_ENV = 'production';
 const request = require('supertest');
+const expect = require('chai').expect;
 require('dotenv').config({ silent: true });
 
-
-const describeOrSkip = process.env.ASSISTANT_ID ? describe : describe.skip;
-describeOrSkip('express', () => {
+describe('express', () => {
   const app = require('../../index');
   it('should load home page when GET /', (done) => {
     request(app).get('/').expect(200, done);
   });
 
-  it('should respond with JSON when GET /api/session', (done) => {
+  it('should return a valid result for account 5893', (done) => {
     request(app)
-      .get('/api/session')
+      .get('/bank/validate?value=5893')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, done);
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.result).to.equal('acc123valid');
+        done();
+      });
+  });
+
+  it('should return an invalid result for account 1111', (done) => {
+    request(app)
+      .get('/bank/validate?value=1111')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.result).to.equal('acc123invalid');
+        done();
+      });
   });
 });
